3da05fa392e55b901050db9348e7b18280c90b44
arek <arkadiusz.pomaranski@embiq.com>
2021-12-28 15:57:37 +0100

    Load and download finished file complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 // @ts-ignore
 import * as fx from 'glfx-es6';
-
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+@Injectable()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  url: any;
+  url: string | ArrayBuffer | null = null;
   selectedFile: Blob = new Blob();
   reader = new FileReader();
   canvas: any;
+  texture: any;
+  filename: string = '';
+  isImageVisible$ = new BehaviorSubject<boolean>(false);
 
   selectImage(event: any) {
+    if (this.canvas) {
+      this.canvas.remove();
+      this.isImageVisible$.next(false);
+    }
     this.selectedFile = event.files[0];
+    this.filename = event.files[0].name;
     this.reader.readAsDataURL(this.selectedFile);
     this.reader.onload = (_event) => {
       this.url = this.reader.result;
     };
   }
+
   editImage() {
     if (this.selectedFile) {
       this.canvas = fx.canvas();
       let image = document.getElementById('image');
-      let texture = this.canvas.texture(image);
-      this.canvas.draw(texture).ink(0.25).update();
+      this.texture = this.canvas.texture(image);
+      this.canvas.draw(this.texture).update(); //before update use filters
       if (image?.parentNode) {
+        this.canvas.classList.add('mx-auto');
+        this.isImageVisible$.next(true);
         image.parentNode.insertBefore(this.canvas, image);
-        image.parentNode.removeChild(image);
       }
     }
   }
+
   downloadImage() {
     this.canvas.update();
-    var link = document.createElement('a');
-    link.download = 'filename.png';
+    let link = document.createElement('a');
+    let dotIndex = this.filename.indexOf('.');
+    this.filename = this.filename.slice(0, dotIndex);
+    link.download = `${this.filename}-editedByMagApp.png`;
     link.href = this.canvas.toDataURL();
     link.click();
   }
